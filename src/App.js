07919a1de902b5bc73e53450b865665da28e298c
preeti28dec <preetithakur28dec2000@gmail.com>
2022-12-21 13:28:55 +0530

    add new things

--- src/App.js
+++ src/App.js
@@ -1,53 +1,7 @@
-import React, {useState, useEffect} from 'react';
-
-import NewExpense from './components/NewExpense/NewExpense';
-
-import Expenses from './components/Expenses/Expenses';
-
-
-let DUMMY_EXPENSE = [];
-
+import React from "react";
+import ExpenseForm from "./components/NewExpense/ExpenseForm";
 const App = () => {
-    
-    const [expenses, setExpenses] = useState(DUMMY_EXPENSE);
-
-    function fetchData(){
-        fetch('https://techgun.website/sample/api/read.php').then(
-            response => {
-                return response.json();
-            }
-        ).then(
-            data => {
-                //console.log(data);
-                setExpenses(data);
-            }
-        );
-    }
-
-    useEffect(()=>{
-        fetchData();
-    },[]);
-
-    const addExpenseHandler = (expense) => {
-        fetch('https://techgun.website/sample/api/create.php', {
-            method: 'POST',
-            body: JSON.stringify(expense),
-            headers: {
-                'content-Type' : 'application/json'
-            }
-        }).then(
-            response => {
-                fetchData();
-            }
-        );
-    };
-
-    return (
-        <div>
-            <NewExpense onAddExpense={addExpenseHandler} />
-            <Expenses item={expenses} />
-        </div>    
-    );
-}
+  return <ExpenseForm />;
+};
 
 export default App;
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,63 +1,87 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
+const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  const [sheet, setSheet] = useState([]);
+  const submitHandler = () => {
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+    setSheet((s) => [
+      ...s,
+      {
+        Title: enteredTitle,
+        Amount: enteredAmount,
+        Date_: enteredDate,
+      },
+    ]);
+    localStorage.setItem('userData',JSON.stringify(sheet))
+  };
+  console.log(sheet.length);
+  const handleDelete = (s) => {
+    setSheet(sheet.filter((el) => el !== s));
+  };
 
-import './ExpenseForm.css';
-
-const ExpenseForm = (props) => {
-
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
-
-    const titleChangeHandler = (event) => {
-        setEnteredTitle(event.target.value);
-    };
-    const amountChangeHandler = (event) => {
-        setEnteredAmount(event.target.value);
-    };
-    const dateChangeHandler = (event) => {
-        setEnteredDate(event.target.value);
-    };
-
-    const submitHandler = (event) => {
-        event.preventDefault();
-
-        const expenseData = {
-            title: enteredTitle,
-            amount: enteredAmount,
-            date: new Date(enteredDate)
-        }
-
-        props.onSaveExpenseData(expenseData);
-
-        console.log(expenseData);
-
-        setEnteredTitle('');
-        setEnteredAmount('');
-        setEnteredDate('');
-    };
-
-    return (
-        <form onSubmit={submitHandler}>
-            <div className="new-expense__controls">
-                <div className="new-expense__control">
-                    <label>Title</label>
-                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
-                </div>
-                <div className="new-expense__control">
-                    <label>Amount</label>
-                    <input type="number"  value={enteredAmount} min="0.01" step="0.01"  onChange={amountChangeHandler} />
-                </div>
-                <div className="new-expense__control">
-                    <label>Date</label>
-                    <input type="date" value={enteredDate} onChange={dateChangeHandler} />
-                </div>
-            </div>
-            <div className="new-expense__actions">
-                <button type="submit">Add Expense</button>
-            </div>
-        </form>
-    );
-}
+  return (
+    <div className="new-expense">
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            placeholder="Enter Item Name"
+            onChange={(e) => setEnteredTitle(e.target.value)}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            value={enteredAmount}
+            placeholder="Enter Item Amount"
+            min="0.01"
+            step="0.01"
+            onChange={(e) => setEnteredAmount(e.target.value)}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            value={enteredDate}
+            onChange={(e) => setEnteredDate(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit" onClick={submitHandler}>
+          Add Expense
+        </button>
+      </div>
+      <div>
+        {sheet.map((i, ind) => {
+          return (
+            <>
+              <div key={ind} className="">
+                {i.Title}
+                {i.Amount}
+                {i.Date_}
+              </div>
+              <button
+                onClick={() => {
+                  handleDelete(i);
+                }}
+              >
+                delete
+              </button>
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default ExpenseForm;
-
